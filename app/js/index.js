e10a3dbc98e5ab20530f2c0d9d3e8f0c5e6fe512
eliot <eliotchareyron@gmail.com>
2016-11-28 15:46:53 +0100

    Mise a jour menu + correction erreur

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,12 @@
-// listen for scroll
-var positionElementInPage = $('#nav').offset().top;
-$(window).scroll(
-	function() {
-		if ($(window).scrollTop() &gt;= positionElementInPage) {
-			// fixed
-			$('#nav').addClass("floatable");
-		} else {
-			// relative
-			$('#nav').removeClass("floatable");
-		}
-	}
-);
+$(document).ready(function(){
+    $("#nav-mobile").html($("#nav-main").html());
+    $("#nav-trigger span").click(function(){
+        if ($("nav#nav-mobile ul").hasClass("expanded")) {
+            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+        }
+    });
+});
